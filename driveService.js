9689716f3e84b77e5f6719def68702cb36332454
CHINNA ROYYALA ARUN KUMAR <160038719+cra-arun@users.noreply.github.com>
2025-07-01 17:09:50 +0530

    Update driveService.js

--- driveService.js
+++ driveService.js
@@ -1,10 +1,11 @@
 const { google } = require('googleapis');
 
-const auth = new google.auth.JWT({
-  email: process.env.GOOGLE_CLIENT_EMAIL,
-  key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  scopes: ['https://www.googleapis.com/auth/drive']
-});
+const auth = new google.auth.JWT(
+  process.env.GOOGLE_CLIENT_EMAIL,
+  null,
+  process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  ['https://www.googleapis.com/auth/drive']
+);
 
 const drive = google.drive({ version: 'v3', auth });
 
